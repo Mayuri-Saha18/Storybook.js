2080404c0ec64c5607678147d55f274014ae2b53
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-09-05 18:27:03 +0530

    add

--- my-app/src/components/Stack/Stack.stories.jsx
+++ my-app/src/components/Stack/Stack.stories.jsx
@@ -3,13 +3,19 @@ import Stack from "./Stack"
 export default {
   title: "Components/Stack",
   component: Stack,
+  parameters:{
+    layout:'centered',
+    },
+tags:['autodocs'],
   argTypes: {
     numberOfChildren: { type: "number", defaultValue: 4 },
   },
 }
 
 const Template = ({ numberOfChildren, ...args }) => (
+
   <Stack {...args}>
+
     {[...Array(numberOfChildren).keys()].map(n => (
       <div
         style={{
@@ -19,10 +25,12 @@ const Template = ({ numberOfChildren, ...args }) => (
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-        }}
-      >
+
+        }}>
         {n + 1}
+
       </div>
+
     ))}
   </Stack>
 )
